f78ec96d3e725107f46960e2b3881420cdeec83d
tokileecy <tokileecy@gmail.com>
2022-01-23 10:41:12 +0000

    feat: add Cancel

---
+++ packages/ui/src/Cancel/Cancel.styles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/css'
+
+export const root = css`
+  rect {
+    fill: none;
+    stroke: #23d7ff;
+  }
+
+  path {
+    fill: #23d7ff;
+  }
+`
---
+++ packages/ui/src/Cancel/Cancel.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as styles from './Cancel.styles'
+
+export type CancelProps = Record<string, unknown>
+
+const Cancel = (): JSX.Element => {
+  return (
+    <svg viewBox="0 0 27 27" version="1.1" className={styles.root}>
+      <rect x="1.303" y="1.201" width="24" height="24" />
+      <path d="M19.972,5.573l-14.296,14.297l0.958,0.958l14.296,-14.296l-0.958,-0.959Z" />
+      <path d="M5.676,6.532l14.296,14.296l0.958,-0.958l-14.296,-14.297l-0.958,0.959Z" />
+    </svg>
+  )
+}
+
+export default Cancel
