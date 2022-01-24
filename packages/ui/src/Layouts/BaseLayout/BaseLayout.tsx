9181f721f6a1d174c1777bda611a2a08d662bb7c
tokileecy <tokileecy@gmail.com>
2022-01-24 06:43:39 +0000

    feat: add classes to BaseLayout

--- packages/ui/src/Layouts/BaseLayout/BaseLayout.tsx
+++ packages/ui/src/Layouts/BaseLayout/BaseLayout.tsx
@@ -5,6 +5,7 @@ import React, {
   useCallback,
   useEffect,
 } from 'react'
+import { cx } from '@emotion/css'
 import * as styles from './BaseLayout.styles'
 import ThreeGraphic from '../../ThreeGraphic'
 import ThreeManager from '../../ThreeGraphic/core/ThreeManager'
@@ -13,11 +14,21 @@ import ThreeCSSLayer from '../../ThreeGraphic/ThreeCSSLayer'
 import UILayer from '../../ThreeGraphic/UILayer'
 import useForwardedRef from '@psycholog-studio/utils/hooks/useForwardedRef'
 
+export type BaseLayoutClassesKey =
+  | 'root'
+  | 'uiLayer'
+  | 'cssLayer'
+  | 'webglLayer'
+
 export interface BaseLayoutProps {
   children?: ReactNode
   threeManagerRef?: MutableRefObject<ThreeManager | null>
   threeManager?: ThreeManager
   cssLayerContent?: ReactNode | undefined
+  className?: string
+  classes?: {
+    [k in BaseLayoutClassesKey]?: string
+  }
 }
 
 const BaseLayout = (props: BaseLayoutProps): JSX.Element => {
@@ -26,6 +37,8 @@ const BaseLayout = (props: BaseLayoutProps): JSX.Element => {
     threeManagerRef = null,
     children,
     cssLayerContent,
+    className,
+    classes = {},
   } = props
   const [cssLayerScene, setCssLayerScene] = useState<THREE.Scene | undefined>(
     undefined
@@ -55,16 +68,23 @@ const BaseLayout = (props: BaseLayoutProps): JSX.Element => {
   }, [cssLayerScene])
 
   return (
-    <main className={styles.root}>
+    <main className={cx(styles.root, classes.root, className)}>
       <ThreeGraphic
         ref={targetThreeManagerRefCallback}
         threeManager={threeManager}
       >
-        <UILayer className={styles.uiLayer}>{children}</UILayer>
-        <ThreeCSSLayer className={styles.threeCssLayer} scene={cssLayerScene}>
+        <UILayer className={cx(styles.uiLayer, classes?.uiLayer)}>
+          {children}
+        </UILayer>
+        <ThreeCSSLayer
+          className={cx(styles.threeCssLayer, classes.cssLayer)}
+          scene={cssLayerScene}
+        >
           {cssLayerContent}
         </ThreeCSSLayer>
-        <ThreeWebglLayer className={styles.threeWebglLayer} />
+        <ThreeWebglLayer
+          className={cx(styles.threeWebglLayer, classes.webglLayer)}
+        />
       </ThreeGraphic>
     </main>
   )
